1f2f33dd65ee177b0976288f85ac638fdfbb6fe9
Mig Salazar <mig.salazars@gmail.com>
2018-07-10 20:51:37 -0500

    week03-lesson04: seed of ex01 added

---
+++ week03/lesson04/solutions/seed-exercise01/CodeToTest/CodeToTest.Code/Booking.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeToTest.Code
+{
+    public class Booking
+    {
+        public User User { get; set; }
+
+        public bool CanCancel(User user)
+        {
+            if (user.IsAdmin)
+                return true;
+
+            if (this.User == user)
+                return true;
+
+            return false;
+        }
+    }
+}
